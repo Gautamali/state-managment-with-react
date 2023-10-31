0acb2402d254c5c2f8ade95a12ef5eccfb1a03dc
Gautamali <gautammali1503@gmail.com>
2023-10-31 22:49:44 +0530

    Successfully Added zustand.js

---
+++ zustand.js
@@ -0,0 +1,35 @@
+import create from 'zustand';
+
+const useStore = create((set) => ({
+  count: 0,
+  increment: () => set((state) => ({ count: state.count + 1 })),
+  decrement: () => set((state) => ({ count: state.count - 1 })),
+  reset: () => set({ count: 0 }),
+}));
+
+function Counter() {
+  const count = useStore((state) => state.count);
+  const increment = useStore((state) => state.increment);
+  const decrement = useStore((state) => state.decrement);
+  const reset = useStore((state) => state.reset);
+
+  let message = "";
+
+  if (count === 10) {
+    message = "Count reached 10!";
+  } else if (count < 0) {
+    message = "Count is negative!";
+  }
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      {message && <p>{message}</p>}
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default Counter;
